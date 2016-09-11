08ff2b270860557f1f15318a423ddbe48ee4eed8
Eralp Kor <eralp.ny@gmail.com>
2016-09-10 23:11:23 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	var additionalId = [951, 952, 953, 954, 955, 956, 957, 958, 959, 960, 961, 962];
 
 
-	var api = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=6514690fd982377cba22dc9a829e7f80";
+	var api = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=6514690fd982377cba22dc9a829e7f80";
 
   	 $.getJSON(api, function(data){
 
